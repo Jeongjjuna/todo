ca96eb002e90d0b0719af888f898474862f37169
Jeongjihun <wlgns2234@naver.com>
2023-05-07 01:05:48 +0900

    feat: security 설정추가
    security와 h2-console을 함께 사용하기 위한 설정 추가

--- src/main/java/jihun/todo/configuration/SecurityConfig.java
+++ src/main/java/jihun/todo/configuration/SecurityConfig.java
@@ -7,11 +7,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+  /**
+   * security와 h2-console 함께 사용하는법.
+   * https://github.com/HomoEfficio/dev-tips/blob/master/Spring%20Security%EC%99%80%20h2-console%20%ED%95%A8%EA%BB%98%20%EC%93%B0%EA%B8%B0.md
+   */
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            .csrf().ignoringAntMatchers("/h2-console/**").and().headers().frameOptions().sameOrigin().and()
             .formLogin().and()
             .build();
   }
