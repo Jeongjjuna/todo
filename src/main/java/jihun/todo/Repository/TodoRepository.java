9ad2611f25cc87b7077de3bd721b0febf7021f7b
Jeongjihun <wlgns2234@naver.com>
2023-05-09 22:10:25 +0900

    feat: todo 레포지토리 구현
    1. todo 레포지토리 및 todo 엔티티, todo 모델 구현.
    2. soft delete 방식을 위하여 update 쿼리 커스텀.

---
+++ src/main/java/jihun/todo/Repository/TodoRepository.java
@@ -0,0 +1,14 @@
+package jihun.todo.Repository;
+
+import jihun.todo.model.entity.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
+  @Modifying
+  @Query("UPDATE TodoEntity t SET t.deleted = true WHERE t = :todo")
+  void delete(@Param("todo") TodoEntity todoEntity);
+}
---
+++ src/main/java/jihun/todo/model/TodoDto.java
@@ -0,0 +1,11 @@
+package jihun.todo.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoDto {
+  private String content;
+}
