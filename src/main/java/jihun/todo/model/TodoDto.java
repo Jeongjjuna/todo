5f5740e794e739ffc1c3bd4669190006d7f33358
Jeongjihun <wlgns2234@naver.com>
2023-05-10 03:36:45 +0900

    refactor: id 필드추가
    엔티티로부터 id를 받아서 전달할 수 있도록 id 필드 추가

--- src/main/java/jihun/todo/model/TodoDto.java
+++ src/main/java/jihun/todo/model/TodoDto.java
@@ -1,11 +1,21 @@
 package jihun.todo.model;
 
-import lombok.Data;
+import jihun.todo.model.entity.TodoEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class TodoDto {
+  private Integer id;
   private String content;
+
+  public static TodoDto from(TodoEntity todoEntity) {
+    return TodoDto.builder()
+            .id(todoEntity.getId())
+            .content(todoEntity.getContent())
+            .build();
+  }
 }
